f6f316e0798d3e842eba9350c719ffc2f10304cc
74dine <72270756+74dine@users.noreply.github.com>
2026-06-09 12:18:17 +0530

    initial commit

---
+++ Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Server.Runners;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.Sources.Clear();
+
+builder.Services.AddLogging(b =>
+{
+    b.SetMinimumLevel(LogLevel.Trace);
+
+    b.ClearProviders();
+
+    b.AddSimpleConsole(opt =>
+    {
+        opt.ColorBehavior = LoggerColorBehavior.Disabled;
+        opt.SingleLine = true;
+        opt.TimestampFormat = "[HH:mm:ss] ";
+        opt.UseUtcTimestamp = true;
+    });
+
+    b.AddFilter("Microsoft.Hosting", LogLevel.Warning);
+    b.AddFilter("Microsoft.Extensions.Hosting", LogLevel.Warning);
+    b.AddFilter("System", LogLevel.Warning);
+});
+
+builder.Services.AddHostedService<DiscordBot>();
+
+var host = builder.Build();
+
+await host.RunAsync();
---
+++ Server/Runners/DiscordBot.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Server.Runners;
+
+public class DiscordBot : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Hello, World!");
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Goodbye, World!");
+        return Task.CompletedTask;
+    }
+}
